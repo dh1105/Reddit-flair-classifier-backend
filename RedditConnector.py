f44b191ce5262432c1ff783baee5dd5c62d38e3a
Dhruv Verma <dhruv2scs@gmail.com>
2020-04-17 11:38:45 +0530

    Added reddit connector

--- RedditConnector.py
+++ RedditConnector.py
@@ -4,11 +4,11 @@ class RedditConnector(object):
 
     def __init__(self):
         super().__init__()
-        self.reddit = Reddit(client_id = "-xZ8AcnSY-RDGA",
-					client_secret = "jUAVs7W9Qnh9ECET2VuG2bpjFx4",
-					user_agent = "First app",
-					username = "sgtcockmunch",
-					password = "Snowy#150319")
+        self.reddit = Reddit(client_id = "",
+					client_secret = "",
+					user_agent = "",
+					username = "",
+					password = "")
     
     def get_url_details(self, url):
         return self.reddit.submission(url=url)
